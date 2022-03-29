ed007d70bf8f63e1a91a22ba993501473b3776dc
duck-ach <101095613+duck-ach@users.noreply.github.com>
2022-03-29 23:34:29 +0900

    이미지 업로드 창 구현
    html 문서에서 javascript가 왜 구현이 안되는것인가.. 생코 영상 열심히 따라할때는 잘만 됐는데,,? 하핳핳하
    이미지 업로드 창 ..음 꼭 구현해서 나의 홈페이지를 꼭 만들겠다. 
    javascript도 오늘 꼭 해답을 찾고 recommit을 하고 잠을들겠나이다...

---
+++ 0 Main.js
@@ -0,0 +1,40 @@
+// 이미지 업로드 기능
+const browseBtn = document.querySelector('.browse-btn');
+const browseBtn = document.querySelector('#real-input');
+browseBtn.addEventListener('click',()=>{
+    realInput.click();
+}
+
+//이미지 미리보기 기능
+function readInputFile(e){
+    var sel_files = [];
+    
+    sel_files = [];
+    $('#imagePreview').empty();
+    
+    var files = e.target.files;
+    var fileArr = Array.prototype.slice.call(files);
+    var index = 0;
+    
+    fileArr.forEach(function(f){
+    	if(!f.type.match("image/.*")){
+        	alert("이미지 확장자만 업로드 가능합니다.");
+            return;
+        };
+        if(files.length < 11){
+        	sel_files.push(f);
+            var reader = new FileReader();
+            reader.onload = function(e){
+            	var html = `<a id=img_id_${index}><img src=${e.target.result} data-file=${f.name} /></a>`;
+                $('imagePreview').append(html);
+                index++;
+            };
+            reader.readAsDataURL(f);
+        }
+    })
+    if(files.length > 11){
+    	alert("최대 10장까지 업로드 할 수 있습니다.");
+    }
+}
+
+$('#real-input').on('change',readInputFile);
